55e55ab40e6764ec43c47d07f8b4e9635c0cc695
Cameron Johnston <cameron@rootdown.net>
2015-03-07 21:28:05 -0700

    rubocop autocorrect

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
-name             "sabnzbd"
-maintainer       "Alex Howells"
-maintainer_email "alex@howells.me"
-license          "Apache 2.0"
-description      "Installs and configures sabnzbd onto a node"
-version          "1.1.2"
+name 'sabnzbd'
+maintainer 'Alex Howells'
+maintainer_email 'alex@howells.me'
+license 'Apache 2.0'
+description 'Installs and configures sabnzbd onto a node'
+version '1.1.2'
 
-%w{ ubuntu }.each do |os|
+%w(ubuntu).each do |os|
   supports os
 end
 
-depends "bluepill"
-depends "git"
+depends 'bluepill'
+depends 'git'
